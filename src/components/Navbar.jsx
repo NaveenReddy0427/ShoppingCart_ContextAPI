77bce7f971300e78ff94baf027fa9c220b412884
NaveenReddy0427 <naveendeva909@gmail.com>
2024-04-15 07:24:27 +0530

    consume the context using useContext hook for the total value

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "../styles/Total.module.css";
+import { useContext } from 'react'
+import { ItemContext } from "../ItemContext"
 
 function Navbar() {
+
+  const value = useContext(ItemContext)
   return (
     <div className={styles.container}>
-      <h1>Total : &#x20B9; 0</h1>
+      <h1>Total : &#x20B9; {value.total}</h1>
       <h1>Items: 0</h1>
     </div>
   );
