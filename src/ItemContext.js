4180039553de66a44412bea40c0295ac1b5463a4
NaveenReddy0427 <naveendeva909@gmail.com>
2024-04-16 07:58:27 +0530

    create custom hook and write useContext in order to consume the context in same file and bring the logic as well to handleAdd and handleRemove in single to debug easy and to readability as well

--- src/ItemContext.js
+++ src/ItemContext.js
@@ -1,15 +1,36 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { useState } from "react";
 
 export const ItemContext = createContext();
+
+export function useValue(){
+    const value = useContext(ItemContext)
+    return value
+}
 
 function CustomContext(props){
 
     const [ total, setTotal ] = useState('')
     const [ item, setItem ] = useState('')
+    
+    const handleAdd = (price) => {
+        setTotal(total + price)
+        setItem(item + 1)
+      };
+    
+      const handleRemove = (price) => {
+        if(total<=0){
+          return
+        }
+    
+        setTotal(total-price)
+        setItem(item-1)
+      };
+
+
     return(
         <>
-        <ItemContext.Provider value={{total, setTotal, item, setItem}}>
+        <ItemContext.Provider value={{total, item, handleAdd, handleRemove}}>
             {props.children}
         </ItemContext.Provider>
         
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import styles from "../styles/ItemCard.module.css";
-import { useContext } from "react";
-import { ItemContext } from "../ItemContext";
+// import { useContext } from "react";
+// import { ItemContext } from "../ItemContext";
+import { useValue } from "../ItemContext";
 
 function ItemCard({ name, price }) {
 
-  const { item, setItem, total, setTotal } = useContext(ItemContext)
+  const { handleAdd, handleRemove } = useValue()
 
-  const handleAdd = () => {
-    setTotal(total + price)
-    setItem(item + 1)
-  };
+  // const handleAdd = () => {
+  //   setTotal(total + price)
+  //   setItem(item + 1)
+  // };
 
-  const handleRemove = () => {
-    if(total<=0){
-      return
-    }
+  // const handleRemove = () => {
+  //   if(total<=0){
+  //     return
+  //   }
 
-    setTotal(total-price)
-    setItem(item-1)
-  };
+  //   setTotal(total-price)
+  //   setItem(item-1)
+  // };
 
   return (
     <div className={styles.itemCard}>
       <div className={styles.itemName}>{name}</div>
       <div className={styles.itemPrice}>&#x20B9; {price}</div>
       <div className={styles.itemButtonsWrapper}>
-        <button className={styles.itemButton} onClick={() => handleAdd()}>
+        <button className={styles.itemButton} onClick={() => handleAdd(price)}>
           Add
         </button>
-        <button className={styles.itemButton} onClick={() => handleRemove()}>
+        <button className={styles.itemButton} onClick={() => handleRemove(price)}>
           Remove
         </button>
       </div>
